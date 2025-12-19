b47bd403e6fd69bb0c209dfd56076a57159ecd65
nkkmt <naoko020188@gmail.com>
2025-12-19 14:42:56 +0900

    ファビコン追加

--- php/takeout.php
+++ php/takeout.php
@@ -43,6 +43,7 @@ if (!empty($_SESSION['cart'])) {
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0, , viewport-fit=cover">
 	<title>テイクアウト - 本格韓国料理 ソダム</title>
+	<link rel="icon" href="../favicon.ico">
 	<link rel="stylesheet" href="../css/common.css">
 	<link rel="stylesheet" href="../css/takeout.css">
 	<link rel="preconnect" href="https://fonts.googleapis.com">
